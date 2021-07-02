70187b6a18d6156f8194889a423e37fefacec705
Divyanshu Sachan <dsac147@gmail.com>
2021-07-02 11:40:11 +0530

    Updated public files

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 gsap.registerPlugin(ScrollToPlugin, ScrollTrigger, CSSRulePlugin);
 
+const navMenus = document.body.querySelectorAll('.nav-item');
+
 //music button functionality
 
 document.getElementById('nav-toggle').addEventListener('change', function () {
@@ -84,20 +86,6 @@ gsap.to('.anime1', { opacity: 1, duration: 1, y: 0, stagger: 0.6 });
 gsap.from('.header__image', { opacity: 0, scale: 0.8, duration: 1 });
 gsap.to('.image-border', { opacity: 1, duration: 1 });
 
-// //active class togggle
-// document.getElementById("section2").each(function() {
-//   // Content Reveal Animation
-//   ScrollTrigger.create({
-//     trigger: "#section2",
-//     start: "top 80%",
-//     end: "bottom 100px",
-//     onEnter: () => $(this).addClass('active'),
-//     onLeave: () => $(this).removeClass('active'),
-//     onEnterBack: () => $(this).addClass('active'),
-//     onLeaveBack: () => $(this).removeClass('active'),
-//   });
-// });
-
 //projects animation
 var tlp = gsap.timeline({
   scrollTrigger: {
@@ -168,3 +156,97 @@ function submitform() {
   xmlhttp.open('POST', 'insert.php');
   xmlhttp.send(formdata);
 }
+
+// Functions to get scrolled %
+
+var winheight, docheight, trackLength, throttlescroll;
+
+function getDocHeight() {
+  var D = document;
+  return Math.max(
+    D.body.scrollHeight,
+    D.documentElement.scrollHeight,
+    D.body.offsetHeight,
+    D.documentElement.offsetHeight,
+    D.body.clientHeight,
+    D.documentElement.clientHeight,
+  );
+}
+
+function getmeasurements() {
+  winheight =
+    window.innerHeight ||
+    (document.documentElement || document.body).clientHeight;
+  docheight = getDocHeight();
+  trackLength = docheight - winheight;
+}
+function setActiveMenu(activeSection) {
+  for (let i = 0; i < navMenus.length; i++) {
+    navMenus[i].className = 'nav-item';
+  }
+
+  navMenus[activeSection - 1].className = 'nav-item active';
+
+}
+
+function searchActiveMenu(percentageScrolled) {
+  switch (true) {
+    case percentageScrolled < 0:
+      // I'm guessing this is an error
+      break;
+    case percentageScrolled >= 0 && percentageScrolled < 23:
+      setActiveMenu(1);
+      break;
+    case percentageScrolled >= 23 && percentageScrolled < 35:
+      setActiveMenu(2);
+      break;
+    case percentageScrolled >= 35 && percentageScrolled < 49:
+      setActiveMenu(3);
+      break;
+    case percentageScrolled >= 49 && percentageScrolled < 78:
+      setActiveMenu(4);
+      break;
+    case percentageScrolled >= 78 && percentageScrolled < 95:
+      setActiveMenu(5);
+      break;
+    case percentageScrolled >= 95:
+      setActiveMenu(6);
+      break;
+    default:
+      setActiveMenu(1);
+  }
+}
+
+function amountscrolled() {
+  var scrollTop =
+    window.pageYOffset ||
+    (document.documentElement || document.body.parentNode || document.body)
+      .scrollTop;
+  var pctScrolled = Math.floor((scrollTop / trackLength) * 100); // gets percentage scrolled (ie: 80 or NaN if tracklength == 0)
+
+  searchActiveMenu(pctScrolled);
+}
+
+getmeasurements();
+
+window.addEventListener(
+  'resize',
+  function () {
+    getmeasurements();
+  },
+  false,
+);
+
+window.addEventListener(
+  'scroll',
+  function () {
+    clearTimeout(throttlescroll);
+    throttlescroll = setTimeout(function () {
+      // throttle code inside scroll to once every 50 milliseconds
+      amountscrolled();
+    }, 50);
+  },
+  false,
+);
+
+amountscrolled();
